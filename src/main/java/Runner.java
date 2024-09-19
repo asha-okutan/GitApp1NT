4082a3a6f63ed05c91eb031878561cdfa2f6c47f
A. Süha Okutan <asha.okutan@gmail.com>
2024-09-19 16:03:43 +0300

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,8 @@ public class Runner {
     public static void main(String[] args) {
 
         System.out.println("versiyon1 ");
+        System.out.println("versiyon2 ");
+        System.out.println("versiyon2 nin devamı");
 
 
 
